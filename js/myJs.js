00167ae25c7293818f121d1a503eb2412e1bd06c
atuljain10595 <atuljain10595@gmail.com>
2014-09-17 02:49:50 +0530

    This is a small part of my big Frontend World

---
+++ js/myJs.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+	$('#menu li').click(function () {
+
+		if($(this).parent().children().index(this) == 0){
+			$('html, body').animate({
+			scrollTop: $("#column0").offset().top - 120
+			}, 1200);
+
+		}
+		else if($(this).parent().children().index(this) == 1){
+			$('html, body').animate({
+			scrollTop: $("#column1").offset().top -120
+			}, 1200);
+		}
+		else if($(this).parent().children().index(this) == 2){
+			$('html, body').animate({
+			scrollTop: $("#column2").offset().top - 120
+		}, 1200);
+		}
+		else{
+			$('html, body').animate({
+			scrollTop: $("#column3").offset().top - 120
+		}, 1200); 
+			}
+		});
+//		});
+
+    $('html, body').animate({
+        scrollTop: $("#joker").offset().top
+    }, 2000);
+
+});
+
+			
+	
