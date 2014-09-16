8bea32d8043940c521f09f0a2933d34a144b65fb
atuljain10595 <atuljain10595@gmail.com>
2014-09-17 03:07:47 +0530

    my webpage

--- js/myJs.js
+++ js/myJs.js
@@ -5,23 +5,23 @@ $(document).ready(function () {
 		if($(this).parent().children().index(this) == 0){
 			$('html, body').animate({
 			scrollTop: $("#column0").offset().top - 120
-			}, 1200);
+			}, 700);
 
 		}
 		else if($(this).parent().children().index(this) == 1){
 			$('html, body').animate({
 			scrollTop: $("#column1").offset().top -120
-			}, 1200);
+			}, 700);
 		}
 		else if($(this).parent().children().index(this) == 2){
 			$('html, body').animate({
 			scrollTop: $("#column2").offset().top - 120
-		}, 1200);
+		}, 700);
 		}
 		else{
 			$('html, body').animate({
 			scrollTop: $("#column3").offset().top - 120
-		}, 1200); 
+		}, 700); 
 			}
 		});
 //		});
